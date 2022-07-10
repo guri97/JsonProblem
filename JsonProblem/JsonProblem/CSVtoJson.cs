6661099fb9db00a8bfcfc57e91e4929fa11fa73b
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-10 16:27:32 +0530

    [gurpreet] UC4_CSVtoJson

---
+++ JsonProblem/JsonProblem/CSVtoJson.cs
@@ -0,0 +1,104 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+    public class JsonAndCSVOperation
+    {
+        public static void ReadFromCsvWriteIntoJson()
+        {
+            string jsonpath = @"C:\Users\GURPREET SINGH\source\repos\JsonProblem\Files\JsontocsvTextFile.csv";
+            List<Person> list = CSVOperation.CSVDeserialize();
+            //jsonSerialize();
+            string result = JsonConvert.SerializeObject(list);
+            //objcet to string
+
+            using (StreamWriter sw = new StreamWriter(jsonpath))
+            {
+                sw.Write(result);
+            }
+
+        }
+
+        public static void ReadFromJsonWriteIntoCsv()
+        {
+            string csvpath = @"C:\Users\GURPREET SINGH\source\repos\JsonProblem\Files\JsontocsvTextFile.csv";
+            List<Person> persons = JsonDeserialization();
+            csvSerilaization(persons, csvpath);
+
+        }
+        public static List<Person> JsonDeserialization()//string to object
+        {
+            string jsonPath = @"C:\Users\GURPREET SINGH\source\repos\JsonProblem\Files\JsontocsvTextFile.csv";
+            if (FileExist(jsonPath))
+            {
+
+                string JsonData = File.ReadAllText(jsonPath);
+                List<Person> result = JsonConvert.DeserializeObject<List<Person>>(JsonData);
+                if (result.Count != 0)
+                    foreach (Person person in result)
+                    {
+
+                        Console.WriteLine(person.ToString());
+                    }
+                return result;
+
+            }
+            else
+            {
+                Console.WriteLine("File not exist");
+            }
+            return null;
+
+
+
+        }
+        public static bool FileExist(string path)
+        {
+            string InputFile = @"C:\Users\GURPREET SINGH\source\repos\JsonProblem\Files\JsontocsvTextFile.csv";
+            if (File.Exists(InputFile))
+            {
+                Console.WriteLine("File Exist");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("not Exist");
+                return false;
+            }
+        }
+        public static void csvSerilaization(List<Person> person, string csvpath)
+        {
+            try
+            {
+                //csvpath = @"G:\visualstudio\FileIOTextFile\DataOperationCSV.csv";
+                //person = new List<Person>();
+                //person.Add(new Person() { PersonId = 12, Name = "Jivesh", Address = "Kayar" });
+                //person.Add(new Person() { PersonId = 13, Name = "Gaurav", Address = "Bengalaru" });
+                //person.Add(new Person() { PersonId = 14, Name = "Chetan", Address = "Assam" });
+
+
+                using (StreamWriter sw = new StreamWriter(csvpath))
+                {
+
+                    CsvWriter csvWriter = new CsvWriter(sw, CultureInfo.InvariantCulture);
+                    //this has to be written to write the records inside the file
+                    csvWriter.WriteRecords(person);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+    }
+}
--- JsonProblem/JsonProblem/Program.cs
+++ JsonProblem/JsonProblem/Program.cs
@@ -4,17 +4,55 @@
     {
         public static void Main(string[] args)
         {
-            //JSONDataOperation.jsonSerialize();
-            //JSONDataOperation.JsonDeserialization();
+            Console.WriteLine("Enter the choice you want \n 1.JsonSerialization\n 2.jsonDeserializeation\n 3.XMLserialization\n 4.XMLDeserialization\n 5.CSVSerialization\n 6.CSVdeserialization\n 7.ReadFreomCsvWriteToJson\n 8.ReadFromJsonWriteIntoCsv");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
 
-            //XMLDataOperation.xmlSerialize();
-            //XMLDataOperation.XmlDeSerialize(); 
+                    JSONDataOperation.jsonSerialize();
+                    break;
+                case 2:
+                    JSONDataOperation.JsonDeserialization();
+                    break;
+                case 3:
+                    XMLDataOperation.xmlSerialize();
+                    break;
+                case 4:
+                    XMLDataOperation.XmlDeSerialize();
+                    break;
+                case 5:
+                    CSVOperation.CSVSerilaization();
+                    break;
 
-            CSVOperation.CSVSerilaization();
-            CSVOperation.CSVDeserialize();
+                case 6:
+                    CSVOperation.CSVDeserialize();
+                    break;
+                case 7:
+                    JsonAndCSVOperation.ReadFromCsvWriteIntoJson();
+                    break;
+                case 8:
+                    JsonAndCSVOperation.ReadFromJsonWriteIntoCsv();
+                    break;
+
+
+            }
 
 
         }
 
     }
 }
+//Enter the choice you want
+// 1.JsonSerialization
+// 2.jsonDeserializeation
+// 3.XMLserialization
+// 4.XMLDeserialization
+// 5.CSVSerialization
+// 6.CSVdeserialization
+// 7.ReadFreomCsvWriteToJson
+// 8.ReadFromJsonWriteIntoCsv
+//7
+//Id:  12 Name: Jivesh Address:  Kayar
+//Id:  13 Name: Gaurav Address:  Bengalaru
+//Id:  14 Name: Chetan Address:  Assam
